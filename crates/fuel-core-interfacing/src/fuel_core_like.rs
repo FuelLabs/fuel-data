6f91f709c9d110f590a873b1d4288e72d8a2d39c
AJ <jurshsmith@gmail.com>
2025-01-13 23:52:31 +0100

    Introduce `fuel-core-interfacing`
    Abstracts away fuel-core interfaces, adapters,
    types and any other integration detail for integrating
    `fuel-core` in `fuel-data`

---
+++ crates/fuel-core-interfacing/src/fuel_core_like.rs
@@ -0,0 +1,115 @@
+use std::sync::Arc;
+
+use fuel_core::{
+    combined_database::CombinedDatabase,
+    database::{
+        database_description::{on_chain::OnChain, DatabaseHeight},
+        Database,
+    },
+    fuel_core_graphql_api::ports::DatabaseBlocks,
+    state::{
+        generic_database::GenericDatabase, iterable_key_value_view::IterableKeyValueViewWrapper,
+    },
+};
+use fuel_core_importer::ports::ImporterDatabase;
+use fuel_core_storage::transactional::AtomicView;
+use fuel_core_types::{
+    blockchain::consensus::{Consensus, Sealed},
+    fuel_types::BlockHeight,
+};
+use tokio::sync::broadcast::Receiver;
+
+use crate::fuel_core_types::*;
+
+pub type OffchainDatabase =
+    GenericDatabase<IterableKeyValueViewWrapper<fuel_core::fuel_core_graphql_api::storage::Column>>;
+
+/// Interface for `fuel-core` related logic.
+/// This was introduced to simplify mocking and testing the `fuel-streams-publisher` crate.
+#[async_trait::async_trait]
+pub trait FuelCoreLike: Sync + Send {
+    async fn start(&self) -> anyhow::Result<()>;
+    fn is_started(&self) -> bool;
+    async fn await_synced_at_least_once(&self, historical: bool) -> anyhow::Result<()>;
+    async fn stop(&self);
+
+    fn base_asset_id(&self) -> &FuelCoreAssetId;
+    fn chain_id(&self) -> &FuelCoreChainId;
+
+    fn database(&self) -> &CombinedDatabase;
+    fn onchain_database(&self) -> &Database<OnChain> {
+        self.database().on_chain()
+    }
+    fn offchain_database(&self) -> anyhow::Result<Arc<OffchainDatabase>> {
+        Ok(Arc::new(self.database().off_chain().latest_view()?))
+    }
+
+    async fn await_offchain_db_sync(&self, block_id: &FuelCoreBlockId) -> anyhow::Result<()>;
+
+    fn blocks_subscription(&self) -> Receiver<fuel_core_importer::ImporterResult>;
+
+    fn get_latest_block_height(&self) -> anyhow::Result<u64> {
+        Ok(self
+            .onchain_database()
+            .latest_block_height()?
+            .map(|h| h.as_u64())
+            .unwrap_or_default())
+    }
+
+    fn get_receipts(&self, tx_id: &FuelCoreBytes32)
+        -> anyhow::Result<Option<Vec<FuelCoreReceipt>>>;
+
+    #[cfg(not(feature = "test-helpers"))]
+    fn get_consensus(&self, block_height: &BlockHeight) -> anyhow::Result<Consensus> {
+        Ok(self
+            .onchain_database()
+            .latest_view()?
+            .consensus(block_height)?)
+    }
+
+    #[cfg(feature = "test-helpers")]
+    fn get_consensus(&self, block_height: &BlockHeight) -> anyhow::Result<Consensus> {
+        Ok(self
+            .onchain_database()
+            .latest_view()?
+            .consensus(block_height)
+            .unwrap_or_default())
+    }
+
+    #[cfg(not(feature = "test-helpers"))]
+    fn get_block_and_producer(
+        &self,
+        sealed_block: Sealed<FuelCoreBlock>,
+    ) -> (FuelCoreBlock, FuelCoreAddress) {
+        let block = sealed_block.entity.clone();
+        let block_producer = sealed_block
+            .consensus
+            .block_producer(&block.id())
+            .expect("Failed to get Block Producer");
+
+        (block, block_producer.into())
+    }
+
+    #[cfg(feature = "test-helpers")]
+    fn get_block_and_producer(
+        &self,
+        sealed_block: Sealed<FuelCoreBlock>,
+    ) -> (FuelCoreBlock, Address) {
+        let block = sealed_block.entity.clone();
+        let block_producer = sealed_block
+            .consensus
+            .block_producer(&block.id())
+            .unwrap_or_default();
+
+        (block, block_producer.into())
+    }
+
+    fn get_sealed_block_by_height(&self, height: u32) -> Sealed<FuelCoreBlock> {
+        self.onchain_database()
+            .latest_view()
+            .expect("failed to get latest db view")
+            .get_sealed_block_by_height(&height.into())
+            .expect("Failed to get latest block height")
+            .expect("NATS Publisher: no block at height {height}")
+    }
+}
---
+++ crates/fuel-core-interfacing/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod fuel_core;
+pub mod fuel_core_like;
+pub mod fuel_core_types;
+
+pub use fuel_core::FuelCore;
+pub use fuel_core_like::FuelCoreLike;
+
+pub mod types {
+    pub use fuel_core_types::*;
+}
