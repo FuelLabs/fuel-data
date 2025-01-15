2773b238f93acde032392b8ecfae212824a47a4f
AJ <jurshsmith@gmail.com>
2025-01-15 13:20:22 +0100

    Simply feature gate `fuel-core-interfacing` types

--- crates/fuel-core-interfacing/src/lib.rs
+++ crates/fuel-core-interfacing/src/lib.rs
@@ -1,9 +1,19 @@
+#[cfg(feature = "all")]
 pub mod fuel_core;
+
+#[cfg(feature = "all")]
 pub mod fuel_core_like;
 
+#[cfg(feature = "all")]
 pub use fuel_core::FuelCore;
+
+#[cfg(feature = "all")]
 pub use fuel_core_like::FuelCoreLike;
 
+#[cfg(feature = any("all", "types"))]
+pub mod fuel_core_types;
+
+#[cfg(feature = any("all", "types"))]
 pub mod types {
     pub use fuel_core_types::*;
 }
